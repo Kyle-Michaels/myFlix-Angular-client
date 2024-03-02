cf1dae26b41d8134f60b342a91a86974877373bd
Kyle Michaels <kyle.michaels@gmail.com>
2024-03-02 02:24:59 -0800

    created director dialog component

---
+++ src/app/director-dialog/director-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-director-dialog',
+  templateUrl: './director-dialog.component.html',
+  styleUrls: ['./director-dialog.component.scss']
+})
+export class DirectorDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { Name: string, Birth: string, Bio: string }
+  ) { }
+  ngOnInit(): void {
+  }
+}
